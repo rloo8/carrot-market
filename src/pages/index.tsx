de3b9876ee2d48fcb9393366d40330d3d64393e4
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-10-16 09:02:15 +0000

    #1.2 Ring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,9 +66,9 @@ export default function Home() {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-2 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500" />
-              <button className="w-5 h-5 rounded-full bg-indigo-500" />
-              <button className="w-5 h-5 rounded-full bg-teal-500" />
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
             </div>
             <div className="flex items-center space-x-4">
               <button className="bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-geay-500 rounded-lg">
