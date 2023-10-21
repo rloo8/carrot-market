cf6bcfb347c65a782fc529b68ea651fc3c0a691c
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-10-21 18:11:34 +0900

    #2.13 Layout 감싸기

--- src/pages/items/upload.tsx
+++ src/pages/items/upload.tsx
@@ -1,75 +1,81 @@
+import Layout from "@/components/layout";
+
 export default function Upload() {
   return (
-    <div className="px-4 my-16 space-y-5">
-      <div>
-        <label className="w-full h-48 flex items-center justify-center border-2 border-dashed rounded-md border-gray-300 hover:border-orange-500 hover:text-orange-500 cursor-pointer">
-          <svg
-            className="h-12 w-12"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <input className="hidden" type="file" />
-        </label>
-      </div>
-
-      <div>
-        <label
-          className="mb-1 block text-sm font-medium text-gray-700"
-          htmlFor="name"
-        >
-          Name
-        </label>
-        <div className="rounded-md relative flex  items-center shadow-sm">
-          <input
-            id="name"
-            type="text"
-            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            required
-          />
-        </div>
-      </div>
-      <div>
-        <label className="text-sm font-medium text-gray-700" htmlFor="price">
-          Price
-        </label>
-        <div className="relative flex items-center rounded-md shadow-md">
-          <div className="absolute left-0 pl-3 flex items-center justify-center">
-            <span className="text-gray-500 pointer-events-none">$</span>
-          </div>
-          <input
-            className="appearance-none w-full px-7 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            id="price"
-            type="text"
-            placeholder="0.00"
-          />
-          <div className="absolute right-0 pr-3 pointer-events-none">
-            <span className="text-gray-500">USD</span>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <label className="text-sm font-medium text-gray-700">Description</label>
+    <Layout title="업로드" canGoBack>
+      <div className="px-4 space-y-5">
         <div>
-          <textarea
-            className="mt-1 shadow-sm w-full border-gray-300 rounded-md focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            rows={4}
-          />
+          <label className="w-full h-48 flex items-center justify-center border-2 border-dashed rounded-md border-gray-300 hover:border-orange-500 hover:text-orange-500 cursor-pointer">
+            <svg
+              className="h-12 w-12"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <input className="hidden" type="file" />
+          </label>
         </div>
-      </div>
 
-      <button className="w-full mt-4 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm font-medium focus:outline-none">
-        Upload product
-      </button>
-    </div>
+        <div>
+          <label
+            className="mb-1 block text-sm font-medium text-gray-700"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <div className="rounded-md relative flex  items-center shadow-sm">
+            <input
+              id="name"
+              type="text"
+              className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <label className="text-sm font-medium text-gray-700" htmlFor="price">
+            Price
+          </label>
+          <div className="relative flex items-center rounded-md shadow-md">
+            <div className="absolute left-0 pl-3 flex items-center justify-center">
+              <span className="text-gray-500 pointer-events-none">$</span>
+            </div>
+            <input
+              className="appearance-none w-full px-7 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              id="price"
+              type="text"
+              placeholder="0.00"
+            />
+            <div className="absolute right-0 pr-3 pointer-events-none">
+              <span className="text-gray-500">USD</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <label className="text-sm font-medium text-gray-700">
+            Description
+          </label>
+          <div>
+            <textarea
+              className="mt-1 shadow-sm w-full border-gray-300 rounded-md focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              rows={4}
+            />
+          </div>
+        </div>
+
+        <button className="w-full mt-4 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm font-medium focus:outline-none">
+          Upload product
+        </button>
+      </div>
+    </Layout>
   );
 }
--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,49 +1,53 @@
+import Layout from "@/components/layout";
+
 export default function EditProfile() {
   return (
-    <div className="py-16 px-4 space-y-4">
-      <div className="flex items-center gap-3">
-        <div className="w-14 h-14 rounded-full bg-slate-400" />
-        <label
-          htmlFor="photo"
-          className="cursor-pointer py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium"
-        >
-          Change photo
-          <input id="photo" type="file" accept="image/*" className="hidden" />
-        </label>
-      </div>
+    <Layout title="프로필 수정" canGoBack>
+      <div className="px-4 space-y-4">
+        <div className="flex items-center gap-3">
+          <div className="w-14 h-14 rounded-full bg-slate-400" />
+          <label
+            htmlFor="photo"
+            className="cursor-pointer py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium"
+          >
+            Change photo
+            <input id="photo" type="file" accept="image/*" className="hidden" />
+          </label>
+        </div>
 
-      <div className="space-y-1">
-        <label htmlFor="email" className="text-sm font-medium text-gray-500">
-          Email adress
-        </label>
-        <input
-          id="email"
-          type="email"
-          required
-          className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-        />
-      </div>
-
-      <div className="space-y-1">
-        <label htmlFor="phone" className="text-sm font-medium text-gray-500">
-          Phone number
-        </label>
-        <div className="flex rounded-md shadow-sm ">
-          <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
-            +82
-          </span>
+        <div className="space-y-1">
+          <label htmlFor="email" className="text-sm font-medium text-gray-500">
+            Email adress
+          </label>
           <input
-            id="phone"
-            type="number"
+            id="email"
+            type="email"
             required
-            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
         </div>
-      </div>
 
-      <button className="mt-5 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm font-medium focus:outline-none">
-        Update Profile
-      </button>
-    </div>
+        <div className="space-y-1">
+          <label htmlFor="phone" className="text-sm font-medium text-gray-500">
+            Phone number
+          </label>
+          <div className="flex rounded-md shadow-sm ">
+            <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
+              +82
+            </span>
+            <input
+              id="phone"
+              type="number"
+              required
+              className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            />
+          </div>
+        </div>
+
+        <button className="mt-5 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm font-medium focus:outline-none">
+          Update Profile
+        </button>
+      </div>
+    </Layout>
   );
 }
