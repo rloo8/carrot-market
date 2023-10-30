b2cf04ea8ad8af0ad186a8ec58cc6e3d6190ca27
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-10-30 13:28:36 +0900

    #6.2 회원가입, 로그인 로직 코드 정리

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -5,43 +5,18 @@ import client from "@libs/server/client";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone } = req.body;
 
-  let user;
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("찾음");
-    if (!user) {
-      console.log("찾지못했음. 만들겠음");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("찾음");
-    if (!user) {
-      console.log("찾지못했음. 만들겠음");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  }
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
 
   return res.status(200).end();
 }
