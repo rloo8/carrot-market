42847f8ba3d98ff74b2ee2a15cb00de895386196
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-10-30 23:42:07 +0900

    #6.5 twilio 셋업 (phone 로그인 시 토큰 문자로 받기)
    .env 파일에
    TWILIO_SID
    TWILIO_TOKEN
    TWILIO_MSID
    TWILIO_PHONE
    MY_PHONE
    작성하기

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+import twilio from "twilio";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -27,7 +30,17 @@ async function handler(
       },
     },
   });
-  console.log(token);
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!, //phone
+      from: process.env.TWILIO_PHONE,
+      body: `Your login token is ${payload}`,
+    });
+
+    console.log(message);
+  }
 
   return res.json({
     ok: true,
