e46d7f5cc8e0d0f369c677de678173c10be6cec2
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-10-31 13:24:59 +0900

    #6.7 토큰 전송 주석 처리 / 유저 db 삭제 시 토큰 db 자동 삭제 로직 추가

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -33,18 +33,18 @@ async function handler(
   });
 
   if (phone) {
-    const message = await twilioClient.messages.create({
+    /*     const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_MSID,
       to: process.env.MY_PHONE!, //phone
       from: process.env.TWILIO_PHONE,
       body: `Your login token is ${payload}`,
     });
 
-    console.log(message);
+    console.log(message); */
   }
 
   if (email) {
-    const mailOptions = {
+    /* const mailOptions = {
       from: process.env.MAIL_ID,
       to: email,
       subject: "Nomad Carrot Authentication Email",
@@ -63,7 +63,7 @@ async function handler(
       }
     );
     smtpTransport.close();
-    console.log(result);
+    console.log(result); */
   }
 
   return res.json({
